b679dd2c6ef7329d0d93e7b43d089dfa321edb7c
Potlee <deeep.deep@gmail.com>
2015-02-22 14:39:21 -0600

    core

--- core/store.js
+++ core/store.js
@@ -1,19 +1,41 @@
-var emitter = new require('events').EventEmitter;
-
-var cards = {};
-
-var initDeck = function() {
-  ['H', 'D', 'C', 'S'].forEach(function(suit) {
-    [2,3,4,5,6,7,8,9,'J','K','Q','A'].forEach(function(value) {
-      cards[value + suit] = {position: [0,0]};
-    });
-  });
-  fb.set({cards: cards});
+var EventEmitter = require('events').EventEmitter;
+var emitter = new EventEmitter();
+var root = require('./fb');
+var game = root.child('game');
+var User = require('./user');
+var cache = {};
+var reset = function (snapshot) {
+  cache = snapshot.val();
+  rerender();
+  console.log(cache);
 };
 
-initDeck();
-emitter.on('move_card', function() {
+game.on('child_added', reset);
+game.on('child_removed', reset);
+game.on('child_changed', reset);
+game.on('value', reset);
 
+
+var rerender = function() { emitter.emit('render_game', cache); };
+
+emitter.on('start_game', function(users) {
+  var cards = {};
+  ['H', 'D', 'C', 'S'].forEach(function(suit) {
+    [2,3,4,5,6,7,8,9,'J','K','Q','A'].forEach(function(value) {
+      cards[value + suit] = {position: {x:0,y:0,z:0}, faceup: true};
+    });
+  });
+  game.set({cards: cards, users: users, turn: users[0]});
 });
 
-//module.exports = fb;
+emitter.on('move_card', function(card, position) {
+  game.child('cards').child(card).update({ position: position }, rerender);
+});
+emitter.on('move_card_to_hand', function(card) {
+  game.child('cards').child(card).update({ username: User.currentUser.name }, rerender);
+});
+emitter.on('flip_card', function(card) {
+  game.child('cards').child(card).update({ faceup: !cache[card].flipped }, rerender);
+});
+
+module.exports = emitter;
