ef6acbbc1dfd0055aa5e033caf8810e3c298e4e3
Brian Hare <hareb@umich.edu>
2015-02-22 15:07:25 +0100

    cleanup

--- table/table.js
+++ table/table.js
@@ -1,176 +1,294 @@
+$(function() {
 
-        
-        $(function() {
-            $( ".hand" ).sortable({
-                //revert: true
+// Disable highlight text for all but inputs
+$('*:not(:input)').disableSelection();
+
+
+render_game("user1");
+
+		//Takes in a json file and recreats all the cards.
+		//--Need to figure out where the file is coming from
+		function render_game(user/*, game_state*/){
+			//removes all card divs
+			$(".card").remove();
+		
+	
+			//var game = JSON.parse(game_state);
+			var game_state = {
+				turn: 'user1',
+				users: ['user1','user2','...'],
+				cards: {
+					'AS': {
+						position: {
+							x: 100,
+							y: 100,
+							z: 0
+						},
+						username: 'user1', // or 'table'
+						faceup: true // or false
+					},
+					'AH': {
+						position: {
+							x: 14,
+							y: 16,
+							z: 2
+						},
+						username: 'table', // or 'table'
+						faceup: true // or false
+					},
+					'AC': {
+						position: {
+							x: 19,
+							y: 15,
+							z: 1
+						},
+						username: 'table', // or 'table'
+						faceup: true // or false
+					},
+					'AD': {
+						position: {
+							x: 67,
+							y: 5,
+							z: 3
+						},
+						username: 'table', // or 'table'
+						faceup: true // or false
+					}
+				},
+				game: 'game 1'
+			}; 
+			
+			//go through each card and create them.
+			
+			//=== does not change data types of either side
+			//-- Currently only distinguishes between hand and table, ownership
+			//-- should be implemented later (create ownership id by username, then
+			//--(#'username').append( )etc.)
+			$.each(game_state.cards, function(key, value){
+			//do checks for users/table or hand checks here.
+				switch (value.username){
+					case user:
+							$("#hand").append(create_card(key, value.faceup));
+							break;
+						
+					case "table":
+							$("#table").append(create_card(key, value.faceup)
+												.css({"top" : value.position.y+'%',
+													"left" : value.position.x+'%', 
+													"z-index" : value.position.z})
+							);
+							break;
+					default:
+							//The case where someone else owns the card, may have to
+							//consider ownership boxes later but right now don't render the card.
+							//keep count of cards in the users hands.
+				}
+				
+			});
+		}
+			
+		//parameters: The data_card (AS, 1H, 10D, or etc), and whether or not the card is faceup.
+		//returns: a div object of the card. to be appended to whatever div it goes to.
+		//i.e. $("#hand").append(create_card("7D", true));
+		function create_card(data_card, faceup){
+			var rank = parse_rank(data_card);
+			var suit = parse_suit(data_card);
+			var symbol = parse_symbol(suit);
+			
+			var rank_lower = rank.toLowerCase();
+			var back = " back";
+			
+			if(faceup){
+				back = "";
+			}
+			
+			//var str = "<div data-card=" + rank + suit.charAt(0).toUpperCase() + " class=\"card rank-" + rank_lower + " " + suit + back + "\"><span class=\"rank\">" + rank + "</span><span class=\"suit\">&" + suit + ";</span></div>";
+			//return str;
+			return $("<div/>")
+						.attr("data-card", data_card)
+						.addClass("card rank-" + rank_lower + " " + suit + back)
+						.append($("<span/>")
+							.addClass("rank")
+							.text(rank)
+						)
+						.append($("<span/>")
+							.addClass("suit")
+							//.text("&" + suit + ";")
+							.text(symbol)
+					);
+							
+							
+		};
+			
+		//Takes a data_card (AC, 2H, 4C, etc) and gets the rank)
+		function parse_rank(data_card){
+			if(data_card.length == 3){
+				return data_card.charAt(0) + data_card.charAt(1);
+			}
+			else{
+				return data_card.charAt(0);
+			}
+		};
+		
+		//Takes a data_card (AC, 2H, 4C, etc) and gets the suit in text
+		function parse_suit(data_card){
+			switch (data_card.charAt(data_card.length - 1)) {
+				case "C":
+					return "clubs";
+					break;
+					
+				case "D":
+					return "diams";
+					break;
+					
+				case "H":
+					return "hearts";
+					break;
+				
+				case "S":
+					return "spades";
+					break;
+					
+				default:
+					return "";
+					break;
+			}
+		};
+		
+		//Takes a data_card (AC, 2H, 4C, etc) and gets the suit symbol
+		//doing .append("<span/>").text("&diams;") doesn't work, this works
+		//around using that by just using the actual symbol.
+		function parse_symbol(suit){
+			switch (suit) {
+				case "clubs":
+					return "♣";
+					break;
+					
+				case "diams":
+					return "♦";
+					break;
+					
+				case "hearts":
+					return "♥";
+					break;
+				
+				case "spades":
+					return "♠";
+					break;
+					
+				default:
+					return "";
+					break;
+			}
+		};
+		
+
+
+$("#hand").sortable({
+	revert: 100,
+    connectWith: '#hand, .droppable',
+    containment: '#bounds',
+    receive: function(event, ui) {
+		var card = ui.item.data('card');
+		console.log('move_card_to_hand(\''+card+'\')');
+    },
+    beforeStop: function (event, ui) {
+        ui.item.css('position', 'relative');
+    }
+});
+
+$(".droppable").droppable({
+	tolerance: "intersect",
+	out: function (ev, ui) {
+		window.origin = this;
+	},
+    drop: function (ev, ui) {
+		
+		
+        if((window.origin !== null && this != window.origin) || ui.draggable.parent().hasClass('ui-sortable')) {
+            var clone = ui.draggable.clone();
+            ui.draggable.remove();
+            
+            $(clone).css({
+				width: '',
+				height: ''
             });
             
- 			//$( ".dealable li" ).draggable({
-			// $( ".deck li" ).draggable({
-//                 connectToSortable: ".hand",
-//                 helper: "original",
-//                 revert: "false",
-//                 drag: function(){
-//             		var offset = $(this).offset();
-//             		var xPos = offset.left;
-//             		var yPos = offset.top;
-//             		console.log('x: ' + xPos);
-//             		console.log('y: ' + yPos);
-//        			 },
-//        			stop: function(){
-//             		var finalOffset = $(this).offset();
-//             		var finalxPos = finalOffset.left;
-//             		var finalyPos = finalOffset.top;
-//             		console.log('Final X: ' + finalxPos);
-//             		console.log('Final Y: ' + finalyPos);
-//             		var this_class = $(this).attr("div");
-//             		console.log($(this));
-//             	}
-//             });
+            $(this).append(clone);
             
-            //  $( "li" ).draggable({
-//                 connectToSortable: ".hand",
-//                 //connectToSortable: ".table",
-//                 helper: "original",
-//                 revert: "false"
-//             });
+            $(clone).draggable({
+				connectToSortable: '#hand',
+				revert: 'invalid',
+				stack: 'div',
+				containment: '#bounds'
+            });
+            
+            ui.draggable = clone;
+        }
+		
+		window.origin = null;
 
-			var first = true;
-			var xPos, yPos;
+		var left = $(ui.draggable).position().left;
+		var top  = $(ui.draggable).position().top;
+		var percentLeft = (left / $(window).width()) * 100;
+		var precentTop  = (top / $(window).height()) * 100;
+		var position = { left: percentLeft+'%', top: precentTop +'%' };
 
-			$( "li" ).draggable({
-                helper: "original",
-                revert: "false",
-				drag: function(){
-            		var offset = $(this).offset();
-            		var x = offset.left;
-            		var y = offset.top;
-            		if(first) { // beginning position of the card
-            			first = false;
-            			xPos = x;
-            			yPos = y;
-            		}
-            		console.log('x: ' + x);
-            		console.log('y: ' + y);
-       			 },
-       			stop: function(){
-       			
-       				console.log('Begin X: ' + xPos);
-            		console.log('Begin Y: ' + yPos);
-       				first = true;
-            		var finalOffset = $(this).offset();
-            		var finalxPos = finalOffset.left;
-            		var finalyPos = finalOffset.top;
-            		console.log('Final X: ' + finalxPos);
-            		console.log('Final Y: ' + finalyPos);
-            		
-            		// snap to hand
-            		if(finalyPos > 415) {
-            			console.log("snap hand");
-            			$(this).addClass("ui-draggable-handle");
-            			$(this).addClass("ui-sortable-handle");
-            			$(this).removeAttr('style').css("position","relative");
-            			var this_class = $(this).children();
-            			//this_class = $(this).attr("div");
-            			console.log(this_class);
- 						$(this_class).removeClass("ui-draggable-handle");
-            			$(".hand").append(this);
-            			$( ".hand" ).sortable({});
-            		}
-            		
-            		//snap to deck one
-            		else if (finalyPos >= 30 && finalyPos <= 44 && 
-            				 finalxPos >= 8 && finalxPos <= 32) {
-            			$(this).removeAttr('style');
-            			$(this).removeClass("ui-draggable-dragging");
-            			$(".deck").append(this);
-            			$(".one").append(this);
-            			console.log("snap deck one");
-            		}
-            		
-            		// snap to deck two
-            		else if (finalyPos >= 183 && finalyPos <= 195 && 
-            				 finalxPos >= 8 && finalxPos <= 32) {
-            			$(this).removeAttr('style');
-            			$(this).removeClass("ui-draggable-dragging");
-            			$(".deck").append(this);
-            			$(".two").append(this);
-            			console.log("snap deck two");
-            		}
-            		
-            		// free on table
-            		else {
-            			console.log("inside else");
-            			// from deck one
-            			if (yPos >= 30 && yPos <= 44 && 
-            				 xPos >= 8 && xPos <= 32) {
-            				//var victim_ul = document.getElementById('one');
-            				//$(this).clone().insertAfter('.table .ui-draggable');
-							//victim_ul.removeChild(this);
-							
-							// $('.one').each(function() {
-//        					 	// save
-//         					$a=$(this).children('li');
-//         					// Move
-//         					$($a.parent().parent()).after($a);
-//         					// Delete
-//         					$(this).remove();
-//     						});
-							
-							// $(this).removeClass("deck");
-// 							$(this).removeClass("one");
-            				//$(".one").removeChild(this);
-            				//$(this).appendTo('.table');
-            				console.log("snap table from deck one");
-            			}
-            			// from deck two
-            			else if (yPos >= 183 && yPos <= 195 && 
-            				 xPos >= 8 && xPos <= 32) {
-            				 
-            			}
-            			// from hand
-            			else if(yPos > 415) {
-            				console.log("from hand");
-            				$(this).removeClass("ui-sortable-handle");
-            				$(this).removeClass("ui-draggable-handle");
-            				var this_class = $(this).children();
- 							$(this_class).addClass("ui-draggable-handle");
- 							//$(this).appendTo('.table');
- 							
- 							//
- 							
- 							
- 							
- 							
-            			}
-            			// from table
-            			else{}
-            		}
-        		}
-        	});
-            
-            $( "ul, li" ).disableSelection();
-            
-            
-            $( "li" ).not('.ui-sortable-handle').draggable({ handle: "div" });
+		var card = $(ui.draggable).data('card');
+		$(ui.draggable).css(position);  
+		console.log('move_card(\''+card+'\', { left: '+percentLeft+'%, top: '+precentTop+'% })');
+    }
+});
 
-			$("div.card").mousedown(function(event) {
-				switch (event.which) {
-					case 2:
-						//alert('Left Mouse button pressed.');
-						break;
-					case 1:
-						//alert('Middle Mouse button pressed.');
-						
-						break;
-					case 3:
-						//alert('Right Mouse button pressed.');
-						$(this).toggleClass("back");
-						event.preventDefault();
-						break;
-						default:
-						alert('You have a strange Mouse!');
-				}
-			});
+
+
+$('body').bind('contextmenu', function(){ return false });
+    
+$("body").mousedown(function(event) {
+	var el = document.elementFromPoint(event.clientX,event.clientY);
+	
+	var card = false;
+	
+	if($(el).hasClass('card')) {
+		card = true;
+	} else {
+		var parentCard = $(el).parents('.card')[0];
+		if(parentCard) {
+			card = true;
+			el = parentCard;
+		}
+	}
+	
+	if(!card) return;
+	
+	switch (event.which) {
+		case 2:
+			//alert('Left Mouse button pressed.');
+			break;
+		case 1:
+			//alert('Middle Mouse button pressed.');
 			
-        });
+			break;
+		case 3:
+			//alert('Right Mouse button pressed.');
+			$(el).toggleClass("back");
+			event.preventDefault();
+			break;
+		default:
+			alert('You have a strange Mouse!');
+	}
+});
+
+			
+// Make only droppable cards sortable because cards in hand
+// because sortable when we run sortable()
+$('.droppable .card').draggable({
+    connectToSortable: '#hand',
+    revert: 'invalid',
+    stack: 'div',
+    containment: '#bounds'
+});
+
+
+		
+});
