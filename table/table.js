976d9f134766264e525fb564d918218cdba37564
Brian Hare <hareb@umich.edu>
2015-02-24 01:11:33 +0100

    move_card now works. not move_to_hand

--- table/table.js
+++ table/table.js
@@ -97,6 +97,8 @@ emitter.emit('start_game', ["deep"]);
 				}
 				
 			});
+			
+			apply_cards();
 		}
 
 
@@ -200,169 +202,165 @@ emitter.emit('start_game', ["deep"]);
 		};
 		
 
+		function apply_cards() {
+			$("#hand").sortable({
+				revert: 100,
+				connectWith: '#hand, .droppable',
+				containment: '#bounds',
+				receive: function(event, ui) {
+				  var card = ui.item.data('card');
+				  console.log('move_card_to_hand(\''+card+'\')');
+				  emitter.emit('move_card_to_hand', card);
+				},
+				beforeStop: function (event, ui) {
+					ui.item.css('position', 'relative');
+				}
+			});
 
-$("#hand").sortable({
-	revert: 100,
-    connectWith: '#hand, .droppable',
-    containment: '#bounds',
-    receive: function(event, ui) {
-      var card = ui.item.data('card');
-      console.log('move_card_to_hand(\''+card+'\')');
-      emitter.emit('move_card_to_hand', card);
-    },
-    beforeStop: function (event, ui) {
-        ui.item.css('position', 'relative');
-    }
-});
+			$(".droppable").droppable({
+				tolerance: "intersect",
+				out: function (ev, ui) {
+					window.origin = this;
+				},
+				drop: function (ev, ui) {
+					
+					
+					if((window.origin !== null && this != window.origin) || ui.draggable.parent().hasClass('ui-sortable')) {
+						var clone = ui.draggable.clone();
+						ui.draggable.remove();
+						
+						$(clone).css({
+							width: '',
+							height: ''
+						});
+						
+						$(this).append(clone);
+						
+						$(clone).draggable({
+							connectToSortable: '#hand',
+							revert: 'invalid',
+							stack: 'div',
+							containment: '#bounds'
+						});
+						
+						ui.draggable = clone;
+					}
+					
+					window.origin = null;
 
-$(".droppable").droppable({
-	tolerance: "intersect",
-	out: function (ev, ui) {
-		window.origin = this;
-	},
-    drop: function (ev, ui) {
-		
-		
-        if((window.origin !== null && this != window.origin) || ui.draggable.parent().hasClass('ui-sortable')) {
-            var clone = ui.draggable.clone();
-            ui.draggable.remove();
-            
-            $(clone).css({
-				width: '',
-				height: ''
-            });
-            
-            $(this).append(clone);
-            
-            $(clone).draggable({
+					var left = $(ui.draggable).position().left;
+					var top  = $(ui.draggable).position().top;
+					var percentLeft = (left / $(window).width()) * 100;
+					var precentTop  = (top / $(window).height()) * 100;
+					var position = { left: percentLeft+'%', top: precentTop +'%' };
+
+					var card = $(ui.draggable).data('card');
+					$(ui.draggable).css(position);  
+					console.log('move_card(\''+card+'\', { left: '+percentLeft+'%, top: '+precentTop+'% })');
+				emitter.emit('move_card', card, {x: percentLeft, y: precentTop, z: 0});
+				}
+			});
+
+
+
+			$('body').bind('contextmenu', function(){ return false });
+				
+			$("body").mousedown(function(event) {
+				var el = document.elementFromPoint(event.clientX,event.clientY);
+				
+				var card = false;
+				
+				if($(el).hasClass('card')) {
+					card = true;
+				} else {
+					var parentCard = $(el).parents('.card')[0];
+					if(parentCard) {
+						card = true;
+						el = parentCard;
+					}
+				}
+				
+				if(!card) return;
+				
+				switch (event.which) {
+					case 2:
+						//alert('Left Mouse button pressed.');
+						break;
+					case 1:
+						//alert('Middle Mouse button pressed.');
+						
+						break;
+					case 3:
+						//alert('Right Mouse button pressed.');
+						$(el).toggleClass("back");
+						event.preventDefault();
+						break;
+					default:
+						alert('You have a strange Mouse!');
+				}
+			});
+
+						
+			// Make only droppable cards sortable because cards in hand
+			// because sortable when we run sortable()
+			$('.droppable .card').draggable({
 				connectToSortable: '#hand',
 				revert: 'invalid',
 				stack: 'div',
 				containment: '#bounds'
-            });
-            
-            ui.draggable = clone;
-        }
-		
-		window.origin = null;
+			});
 
-		var left = $(ui.draggable).position().left;
-		var top  = $(ui.draggable).position().top;
-		var percentLeft = (left / $(window).width()) * 100;
-		var precentTop  = (top / $(window).height()) * 100;
-		var position = { left: percentLeft+'%', top: precentTop +'%' };
-
-		var card = $(ui.draggable).data('card');
-		$(ui.draggable).css(position);  
-		console.log('move_card(\''+card+'\', { left: '+percentLeft+'%, top: '+precentTop+'% })');
-    emitter.emit('move_card', card, {x: percentLeft, y: precentTop, z: 0});
-    }
-});
-
-
-
-$('body').bind('contextmenu', function(){ return false });
-    
-$("body").mousedown(function(event) {
-	var el = document.elementFromPoint(event.clientX,event.clientY);
-	
-	var card = false;
-	
-	if($(el).hasClass('card')) {
-		card = true;
-	} else {
-		var parentCard = $(el).parents('.card')[0];
-		if(parentCard) {
-			card = true;
-			el = parentCard;
+			// buttons
+			$( '.next' ).click(function() {
+			  console.log('end_turn()');
+			});
+			$( '.finish' ).click(function() {  
+			  $.fn.jAlert({
+			  'title':'Game Complete',
+			   'theme':'success',
+			   'size':'small',
+			   'btn': [
+				 {'label':'Begin New Game', 
+				  'cssClass': 'green', 
+				  'onClick': function(){ 
+						$.fn.jAlert({ 'title': 'Begin Game', 
+									  'message': '#cards to be dealt', 
+									  'theme': 'success',
+									  'size':'small',
+									  'btn': [ 
+										{'label':'Shuffle and Deal', 
+										 'cssClass': 'green',  
+										 'onClick': function(){ 
+											var text = prompt("Number of Cards to be Dealt", "0");
+											console.log('deal_and_shuffle('+text+')');
+										 } 
+										},
+										{'label':'Deal', 
+										 'cssClass': 'green',  
+										 'onClick': function(){ 
+											var text = prompt("Number of Cards to be Dealt", "0");
+											console.log('deal('+text+')'); 
+										 } 
+										},
+										{'label':'Skip',  
+										 'onClick': function(){ console.log('start_game()???'); } 
+										}
+										],
+										 'closeBtn': false,    
+										 'autofocus': 'btn:last'
+									  }); 
+						},
+				 },
+				 {'label':'Return to Lobby', 
+				  'cssClass': 'green',  
+				  'onClick': function(){ console.log('go_to_lobby()'); } 
+				 },
+			  ],
+			   'closeBtn': false,
+			   'autofocus': 'btn:last'
+			});
+			  
+					
+			});
 		}
-	}
-	
-	if(!card) return;
-	
-	switch (event.which) {
-		case 2:
-			//alert('Left Mouse button pressed.');
-			break;
-		case 1:
-			//alert('Middle Mouse button pressed.');
-			
-			break;
-		case 3:
-			//alert('Right Mouse button pressed.');
-			$(el).toggleClass("back");
-			event.preventDefault();
-			break;
-		default:
-			alert('You have a strange Mouse!');
-	}
-});
-
-			
-// Make only droppable cards sortable because cards in hand
-// because sortable when we run sortable()
-$('.droppable .card').draggable({
-    connectToSortable: '#hand',
-    revert: 'invalid',
-    stack: 'div',
-    containment: '#bounds'
-});
-
-// buttons
-$( '.next' ).click(function() {
-  console.log('end_turn()');
-});
-$( '.finish' ).click(function() {  
-  $.fn.jAlert({
-  'title':'Game Complete',
-   'theme':'success',
-   'size':'small',
-   'btn': [
-     {'label':'Begin New Game', 
-      'cssClass': 'green', 
-      'onClick': function(){ 
-     		$.fn.jAlert({ 'title': 'Begin Game', 
-     		              'message': '#cards to be dealt', 
-     		              'theme': 'success',
-     		              'size':'small',
-     		              'btn': [ 
-     		              	{'label':'Shuffle and Deal', 
-      						 'cssClass': 'green',  
-      						 'onClick': function(){ 
-      							var text = prompt("Number of Cards to be Dealt", "0");
-      							console.log('deal_and_shuffle('+text+')');
-      						 } 
-     						},
-     						{'label':'Deal', 
-      						 'cssClass': 'green',  
-      						 'onClick': function(){ 
-      						 	var text = prompt("Number of Cards to be Dealt", "0");
-      						 	console.log('deal('+text+')'); 
-      						 } 
-     						},
-     						{'label':'Skip',  
-      						 'onClick': function(){ console.log('start_game()???'); } 
-     						}
-     						],
-     						 'closeBtn': false,    
-     						 'autofocus': 'btn:last'
-     		              }); 
-     		},
-     },
-     {'label':'Return to Lobby', 
-      'cssClass': 'green',  
-      'onClick': function(){ console.log('go_to_lobby()'); } 
-     },
-  ],
-   'closeBtn': false,
-   'autofocus': 'btn:last'
-});
-  
-		
-});
-
-
-
-
-		
 });
