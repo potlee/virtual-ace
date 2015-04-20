c0e89c650df8e958396ee38bd7c53ce9fece425c
Deep Patel <deeppatel@0587389886.wireless.umich.net>
2015-04-20 17:52:32 -0400

    bundle

--- templates/bundle.js
+++ templates/bundle.js
@@ -12887,6 +12887,7 @@ window.User = {
   },
 
   login: function(username) {
+    localStorage.leapLevel = cache[username].level;
     if(!cache[username])
       throw new Error("This user doesn't exist.");
     console.log(this.onlineUsers());
@@ -12898,6 +12899,15 @@ window.User = {
 
   logout: function() {
     localStorage.username = null;
+  },
+
+  setLevel: function(level) {
+    localStorage.leapLevel = level;
+    users.child(this.currentUser()).update({ level: level});
+  },
+
+  getLevel: function() {
+    return cache[User.currentUser()].level;
   }
 };
 
